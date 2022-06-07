0923feb76d231974d35c666cb77ceb3f291ca730
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-06-07 18:59:26 -0500

    Update about section

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -59,7 +59,7 @@ const About = () => {
         <div className="">
           <div className="w-full h-[356px] m-auto bg-center bg-top bg-[length:190%] bg-[url('https://preview.colorlib.com/theme/knuford/images/xabout.jpg.pagespeed.ic.ROjPXqaKV7.webp')]"></div>
 
-          <div className="flex gap-4 py-10">
+          <div className="flex gap-2 py-10 tab-button">
             <NavLink to="">About me</NavLink>
             <NavLink to="/tab-skills">Skills</NavLink>
             <NavLink to="/tab-experience">Experience</NavLink>
--- src/components/expertiseCard.component.jsx
+++ src/components/expertiseCard.component.jsx
@@ -2,14 +2,13 @@ const ExpertiseCard = ({ expertiseList }) => {
   
   return (
     <>
-      {expertiseList.map(({ title, desc, imgPath, cardPathTo }) => (
+      {expertiseList.map(({ title, desc, imgPath }) => (
         <div key={title} className="bg-white drop-shadow-sm rounded p-6">
           <div className="w-20">
             <img src={(imgPath).default} alt="" />
           </div>
           <h3 className="text-blue-500">{title}</h3>
           <p className="">{desc}</p>
-          <a className="text-blue-500" href={cardPathTo}>Read More</a>
         </div>
       ))}
     </>
